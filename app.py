8517ac1ba2d12e1a32b6aa84e9cde9cb72fa1f80
Jason Mai <maijason112102@gmail.com>
2023-08-02 10:27:15 -0400

    automation, manual form
    A user can now create a manual form, drop down are now considered and changes the template.

--- app.py
+++ app.py
@@ -15,9 +15,18 @@ def home():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
+
+        print(request.form.get('plot_choice1'))
+        print(request.form.get('plot_choice2'))
+        print(request.form.get('plot_choice3'))
+
         
         # input if user leaves a Note:
+        plot_choice1 = request.form.get('plot_choice1')
+        plot_choice2 = request.form.get('plot_choice2')
+        plot_choice3 = request.form.get('plot_choice3')
         note = request.form.get('note')
+        preparer = request.form.get('preparer')
         print(note)
 
         # extract file from form data
@@ -27,17 +36,22 @@ def upload_file():
             file.save(os.path.join('./invoice', file.filename))
         
         keyword_dict = ocr.main() # This is where the magic happens
-        invoice_number = keyword_dict['Invoice Number']
-        invoice_date = keyword_dict['Invoice Date']
+        invoice_number = [keyword_dict['Invoice Number']]
+        invoice_date = [keyword_dict['Invoice Date']]
         po_number = keyword_dict['PO Number']
         item = keyword_dict['Item']
         total = keyword_dict['Total']
         price = keyword_dict['Price']
         gl = keyword_dict['GL']
+
+        while len(invoice_date) != len(item):
+            invoice_date.append('')
+            invoice_number.append('')
+
         #move file from invoice folder to archive folder
         os.rename('./invoice/' + file.filename, './archive/' + file.filename)
 
-        return render_template('template.html',invoice_date=invoice_date,invoice_number=invoice_number,po_number=po_number,item=item, total=total, price=price, gl=gl, note=note, zip=zip)
+        return render_template('template.html', plot_choice1=plot_choice1, plot_choice3=plot_choice3, preparer=preparer,invoice_date=invoice_date,invoice_number=invoice_number,po_number=po_number,item=item, total=total, price=price, gl=gl, note=note, zip=zip)
     else:
         return render_template('index.html')
     
@@ -52,74 +66,6 @@ def admin():
     else:
         items = db.get_item_data()
         return render_template('admin.html', items=items)
-
-# Creating routes for the web application
-
-@app.route('/run')
-def index():
-    """Homepage with links to view each table."""
-    tables = ['Employees', 'Certifications', 'EmployeeCertifications', 'ClinicalEducation', 'EmployeeHealth']
-    return render_template_string("""
-    <h1>Welcome to the Employee Database Website</h1>
-    <p>Select a table to view its contents:</p>
-    <ul>
-    {% for table in tables %}
-        <li><a href="/view/{{ table }}">{{ table }}</a></li>
-    {% endfor %}
-    </ul>
-    """, tables=tables)
-
-@app.route('/view/<string:table_name>')
-def view_table(table_name):
-    """Display the contents of the selected table."""
-    conn = get_db()
-    cursor = conn.cursor()
-    cursor.execute(f"SELECT * FROM {table_name}")
-    rows = cursor.fetchall()
-    columns = [desc[0] for desc in cursor.description]
-    return render_template_string("""
-    <h1>Contents of {{ table_name }}</h1>
-    <table border="1">
-        <thead>
-            <tr>
-            {% for col in columns %}
-                <th>{{ col }}</th>
-            {% endfor %}
-            </tr>
-        </thead>
-        <tbody>
-            {% for row in rows %}
-            <tr>
-                {% for item in row %}
-                <td>{{ item }}</td>
-                {% endfor %}
-            </tr>
-            {% endfor %}
-        </tbody>
-    </table>
-    <br>
-    <a href="/">Back to homepage</a>
-    """, table_name=table_name, columns=columns, rows=rows)
-
-# Save the Flask app to a Python file for user download
-app_code = """
-from flask import Flask, render_template_string, g
-import sqlite3
-
-app = Flask(__name__)
-DATABASE = 'employee_database.db'
-
-# Database setup and routes from above will be included here
-
-if __name__ == '__main__':
-    app.run(debug=True)
-"""
-
-with open("/mnt/data/employee_database_app.py", "w") as f:
-    f.write(app_code)
-
-"/mnt/data/employee_database_app.py"
-
 
 
 
@@ -190,3 +136,79 @@ def manual_upload():
     else:
         return render_template('index.html')
 """
+
+
+
+
+'''
+# Creating routes for the web application
+
+@app.route('/run')
+def index():
+    """Homepage with links to view each table."""
+    tables = ['Employees', 'Certifications', 'EmployeeCertifications', 'ClinicalEducation', 'EmployeeHealth']
+    return render_template_string("""
+    <h1>Welcome to the Employee Database Website</h1>
+    <p>Select a table to view its contents:</p>
+    <ul>
+    {% for table in tables %}
+        <li><a href="/view/{{ table }}">{{ table }}</a></li>
+    {% endfor %}
+    </ul>
+    """, tables=tables)
+
+@app.route('/view/<string:table_name>')
+def view_table(table_name):
+    Display the contents of the selected table.
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM {table_name}")
+    rows = cursor.fetchall()
+    columns = [desc[0] for desc in cursor.description]
+    return render_template_string("""
+    <h1>Contents of {{ table_name }}</h1>
+    <table border="1">
+        <thead>
+            <tr>
+            {% for col in columns %}
+                <th>{{ col }}</th>
+            {% endfor %}
+            </tr>
+        </thead>
+        <tbody>
+            {% for row in rows %}
+            <tr>
+                {% for item in row %}
+                <td>{{ item }}</td>
+                {% endfor %}
+            </tr>
+            {% endfor %}
+        </tbody>
+    </table>
+    <br>
+    <a href="/">Back to homepage</a>
+    """, table_name=table_name, columns=columns, rows=rows)
+
+# Save the Flask app to a Python file for user download
+app_code = """
+from flask import Flask, render_template_string, g
+import sqlite3
+
+app = Flask(__name__)
+DATABASE = 'employee_database.db'
+
+# Database setup and routes from above will be included here
+
+if __name__ == '__main__':
+    app.run(debug=True)
+"""
+
+with open("/mnt/data/employee_database_app.py", "w") as f:
+    f.write(app_code)
+
+"/mnt/data/employee_database_app.py"
+
+
+
+"""
+'''
